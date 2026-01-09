bba9df3d7d517bf91e24764f7e597a7d84866abc
VaibhavG2603 <vaibhavgore530@gmail.com>
2026-01-09 11:19:39 +0530

    added Practice1 file

---
+++ Practice1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the values of a and b:\n");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("The sum is:%d\n",c);
+    return 0;
+
+}
