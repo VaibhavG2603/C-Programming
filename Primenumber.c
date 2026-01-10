2926f18606628d2d2f27c4c146ec4380e6408314
VaibhavG2603 <vaibhavgore530@gmail.com>
2026-01-10 10:59:06 +0530

    prime number file added

---
+++ Primenumber.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{   
+    int n,isPrime=0;
+    printf("enter the value which you wants to check prime number or not:\n");
+    scanf("%d",&n);
+    if(n<=1)
+    {
+        isPrime=0;
+      }
+    else
+    {
+      for(int i=2;i<=n/2;i++)
+      {
+        if(n%i==0)
+        {
+            isPrime=0;
+            break;
+        }
+      }
+    }
+    if(isPrime==1)
+    {
+        printf("%d is Prime Number\n",n);
+    }
+    else{
+        printf("%d is Not Prime Number\n",n);
+    }
+    return 0;
+}
