e36ea048fc9f89f7df2b8db6be15d67684803368
VaibhavG2603 <vaibhavgore530@gmail.com>
2026-01-13 10:21:31 +0530

    use of loop added

---
+++ printalphabets.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+
+int main()
+{  
+   int  i= 65;
+  do
+  {
+    printf("%c ",i);
+    i++;
+  } while (i<=90);
+   /*while(i<='Z'){
+    printf("%c ",i);
+    i++;
+   }*/
+
+    return 0;
+}
