c86bf3d375bf336bd92c61194a221aa76366b842
VaibhavG2603 <vaibhavgore530@gmail.com>
2026-03-03 12:24:51 +0530

    GCD added

---
+++ gcd.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+int rec_gcd(int a,int b){
+    if(b==0){
+        return a;
+    }
+    else{
+        return rec_gcd(b,a%b);
+    }
+}
+int main(){
+    int a,b;
+    printf("Enter a and b:\n");
+    scanf("%d %d",&a,&b);
+    int gcd=rec_gcd(a,b);
+    printf("%d",gcd);
+    return 0;
+}
